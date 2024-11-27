300b2ea5e04c43bc29c8a5a30cc227de266b1eb3
flet1234 <flet1908@gmail.com>
2024-11-27 14:19:20 +0200

    fixed tmdb link, fixed meta in index html

--- movieFav3000/src/components/Footer.jsx
+++ movieFav3000/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className="footerContainer">
             <h3>Backend source :</h3>
-        <a href="https://www.themoviedb.org/">
+        <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">
           <img className="footerTMDBImage" src="/assets/images/tmdb_logo.svg"/>
         </a>
       </div>
